55e38c9f1a5affe427b8018cf082141dc199e9a9
meram-amr <meramamr18@gmail.com>
2026-09-13 23:23:57 +0300

    vite.config updated

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/meram_amr/',
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  }
-})
+  base: "/meram_amr/",
+});
